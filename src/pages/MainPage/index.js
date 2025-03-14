f95a7486d1eb589abbeff2680d7df1b716ee8a23
Roman Naumenko-Vakhitsky <romka.nauma@gmail.com>
2025-03-14 20:03:01 +0200

    feat: init project

---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,11 @@
+import styles from './main-page.module.scss'
+
+function MainPage() {
+  return (
+    <div className={styles.mainPage}>
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default MainPage;
