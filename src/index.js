039cccddef62a4f4da8300050b3b0f6c32eb6e73
Roman Naumenko-Vakhitsky <romka.nauma@gmail.com>
2025-03-14 23:40:10 +0200

    feat: path aliases

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import MainPage from './pages/MainPage';
+import '@styles/index.css';
+import MainPage from '@pages/MainPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
