5d9e253bb15590610ae573aed67aeba7cabb5688
Roman Naumenko-Vakhitsky <romka.nauma@gmail.com>
2025-03-15 01:14:03 +0200

    feat: add styles, add style alias

--- craco.config.js
+++ craco.config.js
@@ -6,6 +6,11 @@ module.exports = {
       "@components": path.resolve(__dirname, "src/components"),
       "@styles": path.resolve(__dirname, "src/styles"),
       "@pages": path.resolve(__dirname, "src/pages"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+    },
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.extensions.push(".ts", ".tsx");
+      return webpackConfig;
     },
   },
 };
